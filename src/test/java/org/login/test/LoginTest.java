da45eaca6244575ed25d209dbfa9f1d8ee320882
Dhanasekaran25 <dhanasekarelex@gmail.com>
2021-11-29 15:16:10 +0530

    Chippy code

--- src/test/java/org/login/test/LoginTest.java
+++ src/test/java/org/login/test/LoginTest.java
@@ -2,6 +2,10 @@ package org.login.test;
 
 public class LoginTest {
 public static void main(String[] args) {
+
+	
+	System.out.println("Chippy Tets 1");
+	System.out.println("Chippy Tets 2");
 	
 }
 }
